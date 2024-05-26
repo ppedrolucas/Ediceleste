69189f6d8b2e40df6dd9967ba8f231ae64497c87
Hematopoese <pp15lucas@gmail.com>
2024-05-26 11:40:20 -0300

    darkMode

--- src/script.js
+++ src/script.js
@@ -4,7 +4,21 @@ function menu() {
   nav.classList.toggle("hidden");
   nav.classList.toggle("active");
 }
+function darkMode() {
+  const corpo = document.documentElement;
+  const img = document.getElementById("switchMode");
 
+  corpo.classList.toggle("dark");
+
+  //substituir imagem
+  if (corpo.classList.contains("dark")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "assets/icons/lightMode.svg"); //adionar ou modificar um atributo
+  } else {
+    //se não, manter imagem normal
+    img.setAttribute("src", "assets/icons/darkMode.svg");
+  }
+}
 /*
 let year = document.getElementById("year");
 let data = new Date();
